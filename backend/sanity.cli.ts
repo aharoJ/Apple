63d943b48d9262b9c2770b9c61d3b77bb8d7c190
Angel J. Haro <haro.j.angel@gmail.com>
2024-04-19 19:37:10 -0700

    sanity backend

--- backend/sanity.cli.ts
+++ backend/sanity.cli.ts
@@ -3,6 +3,6 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: 'mx40ox4h',
-    dataset: 'production'
-  }
+    dataset: 'production',
+  },
 })
